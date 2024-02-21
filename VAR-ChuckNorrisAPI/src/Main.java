71a6ae2a0f7a76cc7a04f397b4d690bf7a904970
VINICIUS ANDRÉ ROSA <vinicius_a_rosa@estudante.sesisenai.org.br>
2024-02-21 20:26:36 -0300

    Iniciando o projeto da API Chuck Norris

---
+++ VAR-ChuckNorrisAPI/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            String piada = obterPiadaCuckNorris();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String obterPiadaCuckNorris() throws IOException {
+        //URL da API do ChuckNorris
+        String apiUrl = "https://api.chucknorris.io/jokes/random";
+
+        //Fazendo a requisição GET para a API
+        HttpURLConnection conexao = (HttpURLConnection) new URL(apiUrl).openConnection();
+        conexao.setRequestMethod("GET");
+
+        //Lendo a resposta da API
+        BufferedReader leitor = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+        StringBuilder resposta = new StringBuilder();
+        String linha;
+        while ((linha = leitor.readLine()) != null) {
+            resposta.append(linha);
+
+        }
+        leitor.close();
+
+        //Extrair a piada do JSON
+        int inicioDoIndice = resposta.indexOf("\"value\":") + ("\"value\":".length());
+        int fimDoIndice = resposta.lastIndexOf("\"");
+        return resposta.substring(inicioDoIndice, fimDoIndice);
+
+
+    }
+
+
+}
